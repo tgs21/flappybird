07310583cf7d9cf444e1501b155a1783de8e5be7
Tony <tony_starkey@hotmail.com>
2014-10-18 11:25:11 +0100

    Added sheep image

--- flappy.js
+++ flappy.js
@@ -7,21 +7,24 @@ var stateActions = { preload: preload, create: create, update: update };
 // - renderer (go for Phaser.AUTO)
 // - element where the game will be drawn ('game')
 // - actions on the game state (or null for nothing)
-var game = new Phaser.Game(790, 400, Phaser.AUTO, 'game', stateActions);
+var game = new Phaser.Game(850, 400, Phaser.AUTO, 'game', stateActions);
 
 /*
  * Loads all resources for the game and gives them names.
  */
 function preload() {
-
+ game.load.image("playerImg", "assets/flappy_batman.png");
 
 }
 
 /*
- * Initialises the game. This function is only called once.
+  Initialises the game. This function is only called once.
  */
 function create() {
-    // set the background colour of the scene
+    game.stage.setBackgroundColor("#19FFFF");
+    game.add.text(30,30,"What a ", { font: "60px verdana", fill: "#666FFF"});
+    game.add.sprite(30,270, "playerImg");
+        // set the background colour of the scene
 }
 
 /*
